3cf7c9c809fe8a0b020898ce5bb9fe1571a4cf14
temuujin-0 <temuujin.o@eschool.mn>
2025-02-14 18:37:06 +0800

    table component created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import DataTable from "./modules/DataTable/index.js";
 import Form from "./modules/Form/index.js";
 import Tree from "./modules/Tree/index.js";
 import DatePicker from "./modules/DatePicker/index.js";
@@ -5,4 +6,4 @@ import DatePickerRange from "./modules/DatePickerRange/index.js";
 import TimePickerRange from "./modules/TimePickerRange/index.js";
 import Select from "./modules/Select/index.js";
 
-export { Form, Tree, DatePicker, DatePickerRange, TimePickerRange, Select };
+export { DataTable, Form, Tree, DatePicker, DatePickerRange, TimePickerRange, Select };
